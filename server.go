e770dffd58145ae1954134710d3cb1e60eebc9c4
DanFaizer <danfaizer@gmail.com>
2019-03-27 08:23:23 +0100

    Create goroutine per connection

--- server.go
+++ server.go
@@ -2,10 +2,19 @@ package gowse
 
 import (
 	"fmt"
+	"sync"
 	"time"
 
 	"golang.org/x/net/websocket"
 )
+
+// Client ...
+type Client struct {
+	ID        string
+	Connetion *websocket.Conn
+	Broadcast chan Message
+	Quit      chan bool
+}
 
 // Message ...
 type Message struct {
@@ -14,82 +23,104 @@ type Message struct {
 
 // Topic ...
 type Topic struct {
-	clients          map[string]*websocket.Conn
-	addClientChan    chan *websocket.Conn
-	removeClientChan chan *websocket.Conn
-	broadcastChan    chan Message
+	clients    map[string]*Client
+	register   chan *websocket.Conn
+	unregister chan *websocket.Conn
+	broadcast  chan Message
+	mu         sync.Mutex
 }
 
 // NewServer ...
 func NewServer(ws *websocket.Conn, t *Topic) {
 	go t.run()
 
-	t.addClientChan <- ws
+	t.register <- ws
 
 	for {
 		var m Message
-
 		err := websocket.JSON.Receive(ws, &m)
 		if err != nil {
-			t.broadcastChan <- Message{err.Error()}
-			t.removeClient(ws)
+			t.unregisterClient(ws)
 			return
 		}
-
+		t.broadcast <- m
 	}
 }
 
 // CreateTopic returns a new Topic object
 func CreateTopic() *Topic {
 	return &Topic{
-		clients:          make(map[string]*websocket.Conn),
-		addClientChan:    make(chan *websocket.Conn),
-		removeClientChan: make(chan *websocket.Conn),
-		broadcastChan:    make(chan Message),
+		clients:    make(map[string]*Client),
+		register:   make(chan *websocket.Conn),
+		unregister: make(chan *websocket.Conn),
+		mu:         sync.Mutex{},
 	}
 }
 
-// run receives from the Topic channels and calls the appropriate Topic method
 func (t *Topic) run() {
 	ticker := time.NewTicker(1 * time.Second)
 	for {
 		select {
-		case conn := <-t.addClientChan:
-			t.addClient(conn)
-		case conn := <-t.removeClientChan:
-			t.removeClient(conn)
-		case m := <-t.broadcastChan:
-			t.Publish(m)
+		case conn := <-t.register:
+			t.registerClient(conn)
+		case conn := <-t.unregister:
+			t.unregisterClient(conn)
 		case <-ticker.C:
 			if len(t.clients) > 0 {
-				t.Publish(Message{Text: "ping"})
+				t.Broadcast(Message{Text: "ping"})
 			}
 		}
 	}
 }
 
-// removeClient removes a conn from the pool
-func (t *Topic) removeClient(conn *websocket.Conn) {
-	_, ok := t.clients[conn.Request().RemoteAddr]
+func (c *Client) listen() {
+	for {
+		select {
+		case message := <-c.Broadcast:
+			err := websocket.JSON.Send(c.Connetion, message)
+			if err != nil {
+				fmt.Println("Error publishing message: ", err)
+			}
+		case <-c.Quit:
+			return
+		}
+	}
+}
+
+func (t *Topic) unregisterClient(conn *websocket.Conn) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
+	c, ok := t.clients[conn.Request().RemoteAddr]
 	if ok {
+		c.Quit <- true
 		delete(t.clients, conn.Request().RemoteAddr)
 	}
 }
 
-// addClient adds a conn to the pool
-func (t *Topic) addClient(conn *websocket.Conn) {
-	t.clients[conn.Request().RemoteAddr] = conn
+func (t *Topic) registerClient(conn *websocket.Conn) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
+	client := &Client{
+		ID:        conn.Request().RemoteAddr,
+		Connetion: conn,
+		Broadcast: make(chan Message),
+		Quit:      make(chan bool),
+	}
+
+	t.clients[conn.Request().RemoteAddr] = client
+
+	go client.listen()
 }
 
-// Publish ...
-func (t *Topic) Publish(m Message) {
-	for _, conn := range t.clients {
-		go func(m Message, conn *websocket.Conn) {
-			err := websocket.JSON.Send(conn, m)
-			if err != nil {
-				fmt.Println("Error publishing message: ", err)
-				return
-			}
-		}(m, conn)
+// Broadcast ...
+func (t *Topic) Broadcast(m Message) {
+	t1 := time.Now()
+	for _, client := range t.clients {
+		client.Broadcast <- m
 	}
+	t2 := time.Now()
+	diff := t2.Sub(t1)
+	fmt.Printf("Difference: %s\n", diff)
 }
